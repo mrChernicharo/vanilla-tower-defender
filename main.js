a8049d9dc2d6b6d522dfa8951c91c6190e4ada37
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2023-01-06 01:04:54 -0300

    tiles

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const G = {
   frameId: 0,
   tick: 0,
+  clock: 0,
   loopTimestamp: 0,
   timestamp: Date.now(),
   mouse: { x: null, y: null },
@@ -9,25 +10,31 @@ const G = {
   towers: [],
   bullets: [],
   tiles: [],
+  selectedTile: null,
+  lastSelectedTile: null,
   isPlaying: false,
   stageNumber: 1,
   waveNumber: null,
   wavesData: [{ s: 0, e: null }],
   sceneRect: null,
+  gameSpeed: 2,
 };
 
 const scene = document.querySelector("#scene");
-const g = document.querySelector("g");
 const test = document.querySelector("#test");
 const playPauseBtn = document.querySelector("#play-pause-btn");
+const gameSpeedForm = document.querySelector("#game-speed-form");
+
 const sceneRect = scene.getBoundingClientRect();
+const tileWidth = sceneRect.width / 5;
 
 scene.onpointermove = (e) => {
   G.mouse = { x: e.offsetX, y: e.offsetY };
 };
 scene.onclick = (e) => {
   G.lastClick = { x: e.offsetX, y: e.offsetY };
-  console.log(G);
+
+  selectTile(e.target.dataset.index);
 };
 playPauseBtn.onclick = (e) => {
   G.isPlaying = !G.isPlaying;
@@ -44,51 +51,164 @@ playPauseBtn.onclick = (e) => {
     G.tick = 0;
   }
 };
+gameSpeedForm.onchange = (e) => {
+  e.preventDefault();
+  console.log(e.target.value);
+  let speed;
+  switch (e.target.value) {
+    case "normal":
+      speed = 2;
+      break;
+    case "fast":
+      speed = 4;
+      break;
+    case "faster":
+      speed = 8;
+      break;
+  }
+  G.gameSpeed = speed;
+};
+
+function selectTile(index) {
+  G.lastSelectedTile = G.selectedTile;
+  G.selectedTile = G.tiles[index];
+  console.log(index);
+  G.tiles[index];
+}
+
+const getTileGrassColor = () => {
+  const random = Math.floor(Math.random() * 8);
+  const greens = [
+    "#090",
+    "#060",
+    "#080",
+    "#070",
+    "#060",
+    "#050",
+    "#040",
+    "#030",
+  ];
+  return greens[random];
+};
+function createGrid(cols = 5, rows = 12) {
+  // prettier-ignore
+  for (let row of Array(rows).fill().map((_,i) => i)) {
+    for (let col of Array(cols).fill().map((_,i) => i)) {
+      const pos = { x: col * tileWidth, y: row * tileWidth }
+      const newTile = {
+        index: row * cols + col,
+        id: `${row}:${col}`,
+        pos,
+        shape: null,
+        init() {
+          const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+          rect.setAttribute('id', this.id)
+          rect.setAttribute('data-index', this.index)
+          rect.setAttribute('x', pos.x)
+          rect.setAttribute('y',  pos.y)
+          rect.setAttribute('height',  tileWidth)
+          rect.setAttribute('width', tileWidth)
+          rect.setAttribute('fill', getTileGrassColor())
+          scene.append(rect)
+        }
+      };
+      newTile.init()
+      G.tiles.push(newTile);
+    }
+  }
+  console.log(G);
+}
+
+function spawnEnemy() {
+  const newEnemy = {
+    id: G.tick,
+    shape: null,
+    hp: Math.random() * 800 + 100,
+    size: 15,
+    pos: {
+      x: 0,
+      y: 0,
+    },
+    init() {
+      this.shape = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "circle"
+      );
+      this.pos.x = sceneRect.width / 2 - this.size / 2;
+      this.shape.setAttribute("id", `enemy-${G.tick}`);
+      this.shape.setAttribute("cx", parseInt(this.pos.x));
+      this.shape.setAttribute("cy", parseInt(this.pos.y));
+      this.shape.setAttribute("r", parseInt(this.size));
+      this.shape.setAttribute("data-hp", this.hp);
+      this.shape.setAttribute("fill", "blue");
+      scene.append(this.shape);
+    },
+    move() {
+      this.shape.setAttribute(
+        "cy",
+        Number(this.shape.getAttribute("cy")) + G.gameSpeed
+      );
+    },
+    die() {
+      this.shape.remove();
+    },
+  };
+
+  newEnemy.init();
+  G.enemies.push(newEnemy);
+}
+
+function createTower(pos) {
+  console.log(pos);
+  const newTower = {
+    id: G.tick,
+    shape: null,
+    pos,
+    init() {
+      this.shape = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "circle"
+      );
+      this.shape.setAttribute("id", `enemy-${G.tick}`);
+      this.shape.setAttribute("cx", parseInt(this.pos.x));
+      this.shape.setAttribute("cy", parseInt(this.pos.y));
+      this.shape.setAttribute("r", 25);
+      this.shape.setAttribute("fill", "orange");
+      scene.append(this.shape);
+    },
+  };
+  newTower.init();
+  G.towers.push(newTower);
+}
 
 function update() {
-  for(let enemy of G.enemies) {
-    enemy.circle.setAttribute('cy', Number(enemy.circle.getAttribute('cy')) + 1)
+  for (let enemy of G.enemies) {
+    enemy.move();
+    enemy.hp -= G.gameSpeed;
+
+    if (enemy.hp <= 0) {
+      enemy.die();
+    }
   }
 }
 
+let waveInterval = 120;
 function runAnimation(frame) {
-  G.tick++;
-  G.loopTimestamp = Date.now() - G.timestamp;
-  // console.log(G.loopTimestamp);
+  waveInterval -= G.gameSpeed;
+  G.tick += G.gameSpeed;
+  G.clock = G.tick / 60;
 
-  // test.setAttribute('cy', Number(test.getAttribute('cy')) + 1)
-
-  
-
-
-  // spawn enemy
-  if (G.tick % 60 === 0) {
-    const newEnemy = {
-      id: G.tick,
-      enemy: "dwarf",
-      circle: null,
-      pos: {
-        x: sceneRect.x + sceneRect.width / 2,
-        y: 250 ,
-      },
-      init() {
-        this.circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle')
-        this.circle.setAttribute('cx', parseInt(this.pos.x))
-        this.circle.setAttribute('cy', parseInt(this.pos.y))
-        this.circle.setAttribute('r', 25)
-        this.circle.setAttribute('fill', 'blue')
-        scene.append(this.circle)
-      },
- 
-    }
-    
-    newEnemy.init()
-    G.enemies.push(newEnemy);
+  if (waveInterval < 0) {
+    console.log({ clock: G.clock, tick: G.tick });
+    spawnEnemy();
+    waveInterval = 120;
   }
-  update()
 
+  update();
 
   if (G.isPlaying) {
     requestAnimationFrame(runAnimation);
   }
 }
+
+createGrid();
