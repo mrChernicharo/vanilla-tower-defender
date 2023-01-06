0ba68d596384fa0ae454ecea454e982923197bd9
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2023-01-06 18:34:55 -0300

    menu grind

--- main.js
+++ main.js
@@ -1,3 +1,77 @@
+const FIRST_WAVE_AT_ROW = 3;
+
+const menuIcons = {
+  trap: [],
+  newPath: [
+    {
+      id: "shovel-right-icon",
+      type: "shovel-right",
+      x: 150,
+      y: 75,
+      color: "red",
+      img: "assets/shovel.svg",
+    },
+    {
+      id: "shovel-left-icon",
+      type: "shovel-left",
+      x: 0,
+      y: 75,
+      color: "red",
+      img: "assets/shovel.svg",
+    },
+    {
+      id: "shovel-bottom-icon",
+      type: "shovel-bottom",
+      x: 75,
+      y: 150,
+      color: "red",
+      img: "assets/shovel.svg",
+    },
+  ],
+  tower: [],
+  newTower: [
+    {
+      id: "fire-tower-add-icon",
+      type: "fire",
+      x: 75,
+      y: 0,
+      color: "red",
+      img: "assets/fire.svg",
+    },
+    {
+      id: "lightning-tower-add-icon",
+      type: "lightning",
+      x: 150,
+      y: 75,
+      color: "gold",
+      img: "assets/bolt.svg",
+    },
+    {
+      id: "ice-tower-add-icon",
+      type: "ice",
+      x: 0,
+      y: 75,
+      color: "dodgerblue",
+      img: "assets/snowflake.svg",
+    },
+    {
+      id: "earth-tower-add-icon",
+      type: "earth",
+      x: 75,
+      y: 150,
+      color: "orange",
+      img: "assets/mountain.svg",
+    },
+  ],
+};
+
+const menuActions = {
+  trap: function () {},
+  newPath: handleCreateNewPath,
+  newTower: handleShowTowerPreview,
+  tower: function () {},
+};
+
 const G = {
   frameId: 0,
   tick: 0,
@@ -10,12 +84,13 @@ const G = {
   towers: [],
   bullets: [],
   tiles: [],
+  tileChain: [],
   selectedTile: null,
   lastSelectedTile: null,
   isPlaying: false,
   stageNumber: 1,
   waveNumber: null,
-  wavesData: [{ s: 0, e: null }],
+  wavesTimes: [{ s: 0, e: null }],
   sceneRect: null,
   gameSpeed: 2,
 };
@@ -25,21 +100,23 @@ const scene = document.querySelector("#scene");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const gameSpeedForm = document.querySelector("#game-speed-form");
 const selectionRing = document.querySelector("#selection-ring");
+const selectionRingG = document.querySelector("#selection-ring-g");
 let playPauseIcon = "▶️";
 
 const sceneRect = svg.getBoundingClientRect();
 const tileWidth = sceneRect.width / 6;
-const margin = tileWidth / 2;
-scene.setAttribute("transform", `translate(${margin},${margin})`);
+const MARGIN = tileWidth / 2;
+scene.setAttribute("transform", `translate(${MARGIN},${MARGIN})`);
 
 svg.onpointermove = (e) => {
   G.mouse = { x: e.offsetX, y: e.offsetY };
 };
 svg.onclick = (e) => {
-  G.lastClick = { x: e.offsetX - margin, y: e.offsetY - margin };
+  G.lastClick = { x: e.offsetX - MARGIN, y: e.offsetY - MARGIN };
   console.log(G.lastClick);
 };
 scene.onclick = (e) => {
+  console.log(e);
   const entity = e.target.dataset.entity;
   if (!entity) return;
 
@@ -87,7 +164,7 @@ gameSpeedForm.onchange = (e) => {
   G.gameSpeed = speed;
 };
 
-const getMiddleX = () => sceneRect.width / 2 - margin;
+const getMiddleX = () => sceneRect.width / 2 - MARGIN;
 const getTileColor = (type, blocked = false) => {
   if (blocked) return "#444";
   if (type === "path") return "#971";
@@ -96,126 +173,118 @@ const getTileColor = (type, blocked = false) => {
   return greens[random];
 };
 
-// <i class="fa-solid fa-coins"></i>
-// <i class="fa-solid fa-close"></i>
-// <i class="fas-solid fa-shovel"></i>
+function handleShowTowerPreview(tile, icon) {
+  console.log("handle tower preview", tile, icon);
+}
 
-function handleTileMenu(tile) {
-  console.log("handleTimeMenu", tile);
+function handleCreateNewPath(tile, icon) {
+  console.log("handleCreateNewPath", tile, icon);
+  // const barrierBroken = tile.y > firstWaveRow + waveNumber;
+}
 
-  Array.from(document.querySelectorAll(".ring-icon")).forEach((circle) =>
-    circle.remove()
-  );
-  Array.from(document.querySelectorAll(".defs")).forEach((defs) =>
-    defs.remove()
-  );
+function handleDisplayTileMenu(tile) {
+  console.log("handleDisplayTileMenu", tile);
 
-  if (tile.blocked) {
-    return;
-  }
-
-  const ring = document.querySelector("#selection-ring-g");
-  const translation = ring.getAttribute("transform");
-  const [x, y] = translation
-    .replace(/translate|\(|\)/g, "")
-    .split(",")
-    .map(Number);
-
-  const menuIcons = {
-    trap: [],
-    newPath: [
-      { x: 75, y: 0, iconClass: null, img: "shovel.svg" },
-      { x: 150, y: 75, iconClass: null, img: "shovel.svg" },
-      { x: 0, y: 75, iconClass: null, img: "shovel.svg" },
-      { x: 75, y: 150, iconClass: null, img: "shovel.svg" },
-    ],
-    tower: [],
-    newTower: [
-      { x: 75, y: 0, iconClass: null, img: "coins.svg" },
-      { x: 150, y: 75, iconClass: null, img: "coins.svg" },
-      { x: 0, y: 75, iconClass: null, img: "coins.svg" },
-      { x: 75, y: 150, iconClass: null, img: "coins.svg" },
-      // { x: 75, y: 0, iconClass: "fa-solid fa-coins" },
-      // { x: 150, y: 75, iconClass: "fa-solid fa-coins" },
-      // { x: 0, y: 75, iconClass: "fa-solid fa-coins" },
-      // { x: 75, y: 150, iconClass: "fa-solid fa-coins" },
-    ],
+  const removeRingIcons = () => {
+    Array.from(document.querySelectorAll(".ring-icon")).forEach((circle) => {
+      circle.removeEventListener("click", menuActions[circle.dataset.type]);
+      circle.remove();
+    });
+    Array.from(document.querySelectorAll(".defs")).forEach((defs) =>
+      defs.remove()
+    );
   };
 
-  let menuType;
-  if (tile.type === "path") {
-    if (tile.connected) {
-      // trap
-      console.log("trap");
-      menuType = "trap";
+  const getMenuType = () => {
+    if (tile.type === "path") {
+      if (tile.connected) {
+        return "trap";
+      } else {
+        return "newPath";
+      }
     } else {
-      // newPath
-      console.log("newPath");
-      menuType = "newPath";
+      if (tile.hasTower) {
+        return "tower";
+      } else {
+        return "newTower";
+      }
     }
-  } else {
-    if (tile.hasTower) {
-      // tower
-      console.log("tower");
-      menuType = "tower";
-    } else {
-      // newTower
-      console.log("newTower");
-      menuType = "newTower";
-    }
-  }
-  console.log({ menuType });
+  };
 
-  for (const [i, item] of menuIcons[menuType].entries()) {
-    const circle = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "circle"
-    );
-    circle.setAttribute("class", `ring-icon`);
-    circle.setAttribute("cx", tile.pos.x + item.x);
-    circle.setAttribute("cy", tile.pos.y + item.y);
-    circle.setAttribute("r", 20);
-    circle.setAttribute("fill", "blue");
+  const drawRingIcons = (menuType) => {
+    // const translation = selectionRingG.getAttribute("transform");
+    // const [x, y] = translation
+    //   .replace(/translate|\(|\)/g, "")
+    //   .split(",")
+    //   .map(Number);
+    const pluckTowerType = (id) => id.split("-")[0];
 
-    // if (item.iconClass) {
-    //   const icon = document.createElement("i");
-    //   icon.setAttribute("class", iconClass);
-    //   icon.setAttribute("cx", tile.pos.x + item.x);
-    //   icon.setAttribute("cy", tile.pos.y + item.y);
-    //   ring.append(icon);
-    // }
+    const icons = [];
+    for (const [i, item] of menuIcons[menuType].entries()) {
+      const circle = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "circle"
+      );
+
+      circle.setAttribute("class", `ring-icon`);
+      circle.setAttribute("data-entity", `ring-icon`);
+      circle.setAttribute("data-type", item.type);
+      circle.setAttribute("id", item.id);
+      circle.setAttribute("cx", tile.pos.x + item.x);
+      circle.setAttribute("cy", tile.pos.y + item.y);
+      circle.setAttribute("r", 20);
+      circle.setAttribute("stroke", item.color);
 
       const defs = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "defs"
       );
-      const pattern =  document.createElementNS(
+      const pattern = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "pattern"
       );
-      const image =  document.createElementNS(
+      const image = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "image"
       );
-      defs.setAttribute('class', 'defs')
-      const patternId = `${tile.id}-bg-img`
-      pattern.setAttribute('id', patternId)
-      pattern.setAttribute('patternUnits','noRepeat')
-      pattern.setAttribute('width',28)
-      pattern.setAttribute('height',28)
-      image.setAttribute('href', item.img)
-      image.setAttribute('x',5)
-      image.setAttribute('y',5)
-      image.setAttribute('width',28)
-      image.setAttribute('height',28)
+      defs.setAttribute("class", "defs");
+      const patternId = `${i}-${tile.id}-bg-img`;
+      pattern.setAttribute("id", patternId);
+      pattern.setAttribute("width", 28);
+      pattern.setAttribute("height", 28);
+      image.setAttribute("href", item.img);
+      image.setAttribute("x", 5);
+      image.setAttribute("y", 5);
+      image.setAttribute("width", 28);
+      image.setAttribute("height", 28);
 
-      pattern.append(image)
-      defs.append(pattern)
-      ring.append(defs)
+      pattern.append(image);
+      defs.append(pattern);
+      selectionRingG.append(defs);
 
-      circle.setAttribute('fill', `url(#${patternId})`)
-    ring.append(circle);
+      circle.setAttribute("fill", `url(#${patternId})`);
+      selectionRingG.append(circle);
+
+      icons.push(circle);
+    }
+    return icons;
+  };
+
+  const appendIconsListeners = (icons) => {
+    icons.forEach(
+      (icon) => (icon.onclick = (e) => menuActions[menuType](tile, icon))
+    );
+  };
+
+  removeRingIcons();
+
+  if (tile.blocked) {
+    return;
   }
+
+  const menuType = getMenuType();
+  const icons = drawRingIcons(menuType);
+  appendIconsListeners(icons);
 }
 
 function handleSelectTile(index) {
@@ -225,19 +294,22 @@ function handleSelectTile(index) {
   if (G.lastSelectedTile?.id === G.selectedTile?.id) {
     G.selectedTile = null;
     G.lastSelectedTile.blur();
+    selectionRingG.setAttribute("style", "opacity: 0");
     selectionRing.setAttribute("style", "opacity: 0");
   } else {
     G.lastSelectedTile?.blur();
     G.selectedTile.focus();
     if (G.selectedTile.blocked) {
+      selectionRingG.setAttribute("style", "opacity: 0");
       selectionRing.setAttribute("style", "opacity: 0");
     }
     if (!G.selectedTile.blocked) {
       const { x, y } = G.selectedTile.pos;
       selectionRing.setAttribute("transform", `translate(${x},${y})`);
-      selectionRing.setAttribute("style", "opacity: 0.5");
+      selectionRing.setAttribute("style", "opacity: .75");
+      selectionRingG.setAttribute("style", "opacity: 1");
     }
-    handleTileMenu(G.selectedTile);
+    handleDisplayTileMenu(G.selectedTile);
   }
 }
 
@@ -260,13 +332,18 @@ function createGrid(cols = 5, rows = 12) {
         connected: false,
         hasTower: false,
         blocked: false,
+        startingPoint: false,
+        exits: null,
         init() {
           this.shape = document.createElementNS(
             "http://www.w3.org/2000/svg",
             "rect"
           );
-          this.type = isInitialPath(row, col) ? 'path' : 'grass';
+          const isStartingPoint = isInitialPath(row,col);
+          this.type = isStartingPoint ? 'path' : 'grass';
           this.blocked = isBlocked(row, col);
+          this.startingPoint = isStartingPoint
+          this.exits = isStartingPoint ? getTileExits(this): null;
           this.fill = getTileColor(this.type, this.blocked);
           this.shape.setAttribute("id", this.id);
           this.shape.setAttribute("data-entity", "tile");
@@ -398,3 +475,52 @@ setTimeout(() => {
   G.frameId = requestAnimationFrame(runAnimation);
   console.log("auto-play");
 }, 1000);
+
+function getTileExits(tile) {
+  if (tile.startingPoint) {
+    console.log("hey", tile, G.tiles, G.tileChain);
+    return {
+      left: { x: tile.pos.x + 25, y: 0 },
+      center: { x: tile.pos.x + 50, y: 0 },
+      right: { x: tile.pos.x + 75, y: 0 },
+    };
+  }
+
+  // return;
+  const prevTile = G.tileChain.at(-1);
+
+  const left = { x: 0, y: 0 };
+  const center = { x: 0, y: 0 };
+  const right = { x: 0, y: 0 };
+
+  // newTile below
+  if (prevTile.pos.y < tile.pos.y) {
+    left.x = tile.pos.x + 25;
+    left.y = tile.pos.y;
+    center.x = tile.pos.x + 50;
+    center.y = tile.pos.y;
+    right.x = tile.pos.x + 75;
+    right.y = tile.pos.y;
+  }
+
+  // newTile to the left
+  if (prevTile.pos.x > tile.pos.x) {
+    left.x = tile.pos.x + 100;
+    left.y = tile.pos.y + 25;
+    center.x = tile.pos.x + 100;
+    center.y = tile.pos.y + 50;
+    right.x = tile.pos.x + 100;
+    right.y = tile.pos.y + 75;
+  }
+
+  // newTile to the right
+  if (prevTile.pos.x < tile.pos.x) {
+    left.x = tile.pos.x;
+    left.y = tile.pos.y + 75;
+    center.x = tile.pos.x;
+    center.y = tile.pos.y + 50;
+    right.x = tile.pos.x;
+    right.y = tile.pos.y + 25;
+  }
+  return { left, center, right };
+}
