6469bcff4a587899fb4950cab0f51b58951b073a
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2023-02-21 15:43:26 -0300

    lightning bullet looks ok

--- src/lib/bullets.js
+++ src/lib/bullets.js
@@ -24,79 +24,83 @@ export function createBullet(tower, enemy) {
         "d",
         `M ${this.pos.x} ${this.pos.y} L ${this.enemy.pos.x} ${this.enemy.pos.y}`
       );
-      // this.path.setAttribute("stroke-width", 2);
-      // this.path.setAttribute("stroke", "gray");
 
-      this.shape = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "circle"
-      );
-      this.shape.setAttribute("r", 24);
-      this.shape.setAttribute("stroke", "green");
-      this.shape.setAttribute("stroke-width", 1);
-      this.shape.setAttribute("cx", this.pos.x);
-      this.shape.setAttribute("cy", this.pos.y);
-      // this.shape.setAttribute("transform", 'translate(30,50)');
-      // this.shape.setAttribute("fill", TOWERS[this.type].fill);
+      if (this.type === "lightning") {
+        this.path.setAttribute("stroke-width", 2);
+        this.path.setAttribute("stroke", "gold");
 
-      const patternId = `bullet-pattern-${this.id}`;
-      const bulletImgs = {
-        fire: "/assets/sprites/bullet.svg",
-        ice: "/assets/sprites/ice-bullet.svg",
-        lightning: "/assets/sprites/bullet.svg",
-        earth: "/assets/sprites/earth-bullet.svg",
-      };
-      const translations = {
-        fire: "4px, 4px",
-        ice: "4px, 4px",
-        lightning: "4px, 4px",
-        earth: "4px, 10px",
-      };
-      const angle = getAngle(
-        tower.pos.x,
-        tower.pos.y,
-        enemy.pos.x,
-        enemy.pos.y
-      );
+        bulletsG.append(this.path);
+      }
 
-      const bulletImg = bulletImgs[this.type];
-      const imgTransform = `translate(${
-        translations[this.type]
-      }) rotate(${angle}deg)`;
+      if (this.type !== "lightning") {
+        this.shape = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "circle"
+        );
+        this.shape.setAttribute("r", 24);
+        this.shape.setAttribute("stroke", "green");
+        this.shape.setAttribute("stroke-width", 1);
+        this.shape.setAttribute("cx", this.pos.x);
+        this.shape.setAttribute("cy", this.pos.y);
+        // this.shape.setAttribute("transform", 'translate(30,50)');
+        // this.shape.setAttribute("fill", TOWERS[this.type].fill);
 
-      // this.shape.setAttribute("fill", TOWERS[this.type].fill);
-      this.shape.setAttribute("fill", `url(#${patternId})`);
+        const patternId = `bullet-pattern-${this.id}`;
+        const bulletImgs = {
+          fire: "/assets/sprites/bullet.svg",
+          ice: "/assets/sprites/ice-bullet.svg",
+          earth: "/assets/sprites/earth-bullet.svg",
+        };
+        const translations = {
+          fire: "4px, 4px",
+          ice: "4px, 4px",
+          earth: "4px, 10px",
+        };
+        const angle = getAngle(
+          tower.pos.x,
+          tower.pos.y,
+          enemy.pos.x,
+          enemy.pos.y
+        );
 
-      this.defs = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "defs"
-      );
-      const pattern = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "pattern"
-      );
-      const image = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "image"
-      );
-      this.defs.setAttribute("id", `bullet-defs-${this.id}`);
-      this.defs.setAttribute("class", "defs bullet-defs");
-      pattern.setAttribute("id", patternId);
-      pattern.setAttribute("width", 1);
-      pattern.setAttribute("height", 1);
-      image.setAttribute("href", bulletImg);
-      image.setAttribute("id", `bullet-image-${this.id}`);
-      image.setAttribute("class", `bullet-image`);
-      image.setAttribute("width", 40);
-      image.setAttribute("height", 40);
+        const bulletImg = bulletImgs[this.type];
+        const imgTransform = `translate(${
+          translations[this.type]
+        }) rotate(${angle}deg)`;
 
-      image.style.transform = imgTransform;
+        // this.shape.setAttribute("fill", TOWERS[this.type].fill);
+        this.shape.setAttribute("fill", `url(#${patternId})`);
 
-      pattern.append(image);
-      this.defs.append(pattern);
-      bulletsG.append(this.defs);
-      bulletsG.append(this.shape);
-      // bulletsG.append(this.path);
+        this.defs = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "defs"
+        );
+        const pattern = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "pattern"
+        );
+        const image = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "image"
+        );
+        this.defs.setAttribute("id", `bullet-defs-${this.id}`);
+        this.defs.setAttribute("class", "defs bullet-defs");
+        pattern.setAttribute("id", patternId);
+        pattern.setAttribute("width", 1);
+        pattern.setAttribute("height", 1);
+        image.setAttribute("href", bulletImg);
+        image.setAttribute("id", `bullet-image-${this.id}`);
+        image.setAttribute("class", `bullet-image`);
+        image.setAttribute("width", 40);
+        image.setAttribute("height", 40);
+
+        image.style.transform = imgTransform;
+
+        pattern.append(image);
+        this.defs.append(pattern);
+        bulletsG.append(this.defs);
+        bulletsG.append(this.shape);
+      }
     },
     move() {
       const nextPos = this.path.getPointAtLength(
@@ -107,17 +111,22 @@ export function createBullet(tower, enemy) {
         "d",
         `M ${this.pos.x} ${this.pos.y} L ${this.enemy.pos.x} ${this.enemy.pos.y}`
       );
-      this.shape.setAttribute("cx", this.pos.x);
-      this.shape.setAttribute("cy", this.pos.y);
+
+      if (this.type !== "lightning") {
+        this.shape.setAttribute("cx", this.pos.x);
+        this.shape.setAttribute("cy", this.pos.y);
+      }
     },
     hit(enemy) {
       enemy.hp -= this.damage;
       this.remove();
     },
     remove() {
-      this.shape.remove();
+      if (this.type !== "lightning") {
+        this.shape.remove();
+        this.defs.remove();
+      }
       this.path.remove();
-      this.defs.remove();
       G.bullets = G.bullets.filter((b) => b.id !== this.id);
       // const idx = G.bullets.findIndex((b) => b.id === this.id);
       // G.bullets.splice(idx, 1);
